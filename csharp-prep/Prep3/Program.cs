9b464702623b22510906f072f6ef603fe76e63e1
cardenv <113810540+cardenv@users.noreply.github.com>
2023-09-16 15:51:05 -0600

    Week1 C# Prove

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("Please enter the magic number: ");
+        // string magicNumber = Console.ReadLine();
+        // int numMagic = int.Parse(magicNumber);
+
+        // Console.Write("What is your guess: ");
+        // string numberGuess = Console.ReadLine();
+        // int numGuess = int.Parse(numberGuess);
+
+        Random randomGenerator = new Random();
+        int numMagic = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != numMagic)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > numMagic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < numMagic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+        }
     }
 }
