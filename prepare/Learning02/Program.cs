f288efccc998da5652c9b74d8429733e01638bec
cardenv <113810540+cardenv@users.noreply.github.com>
2023-09-18 18:37:01 -0600

    Week 02 Completed Prepare Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Insurance Agent";
+        job1._company = "IBI";
+        job1._startYear = 2018;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Center Manager";
+        job2._company = "Teleperformance";
+        job2._startYear = 2013;
+        job2._endYear = 2018;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Carden Allen";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
     }
 }
