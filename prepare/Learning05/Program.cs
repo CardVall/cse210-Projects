2364b23a28a5d3e8468ffbfd861621247b2cb0e8
cardenv <113810540+cardenv@users.noreply.github.com>
2023-10-11 13:28:11 -0600

    Wk05 Learning Activity Complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+       List<Shape> shapes = new List<Shape>();
+
+       Square sq1 = new Square("blue", 4); 
+       shapes.Add(sq1);
+
+       Rectangle rt1 = new Rectangle("green", 5, 2);
+       shapes.Add(rt1);
+
+       Circle cr1 = new Circle("pink", 3);
+       shapes.Add(cr1);
+
+       foreach (Shape shape in shapes)
+       {
+        string color = shape.GetColor();
+
+        double  area  = shape.GetArea();
+        
+        Console.WriteLine($"The {color} shape has an area of {area}.");
+       }
     }
 }
